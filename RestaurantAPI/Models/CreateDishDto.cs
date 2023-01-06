9f74eceb416b5e61926ab0608fb5dda1377e34f4
PanJakub97 <110171688+PanJakub97@users.noreply.github.com>
2023-01-06 18:57:13 +0100

    fixbuild

--- RestaurantAPI/Models/CreateDishDto.cs
+++ RestaurantAPI/Models/CreateDishDto.cs
@@ -5,7 +5,7 @@ namespace RestaurantAPI.Models
     public class CreateDishDto
     {
         [Required]
-        public string Name { get; set; }
+        public string Name { get; set }
         public string Description { get; set; }
         public decimal Price { get; set; }
 
